6c41a695a901ed4878ae3fdde5435956989b8e36
irinasyl <92393817+irinasyl@users.noreply.github.com>
2021-10-22 13:56:15 +0300

    Add files via upload

---
+++ Lesson_1/Competition.java
@@ -0,0 +1,11 @@
+package Lesson_1;
+
+public class Competition {
+    public static void main (String [] args) {
+        Team [] team = {new Team("Саша", 100, 180),
+                new Team("Паша", 100, 200),
+                new Team ("Настя", 100, 200),
+                new Team("Валя", 80, 160)};
+        Course [] course = {new Course(100,"Road 1"), new Course(200, "Road 2")};
+    }
+}
